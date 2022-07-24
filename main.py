62286059108ef6ee6b771cdd5dc5c86c0450280d
Willshears <75121539+Willshears@users.noreply.github.com>
2022-07-24 18:14:22 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+
+# make a deck of cards
+import random
+values = range(2, 15)
+suits = "D C H S".split()
+
+deck = ["{0}{1}".format(v, s) for v in values for s in suits]
+print(deck)
+print(len(deck))
+monte_carlo = 10000000
+card_draws = 5
+evaluator = 0
+instances = 0
+for hand in range(monte_carlo):
+    previous_cards = []
+    deck_instance = deck
+    fresh_card = random.choice(deck_instance)
+    multiplier = 0
+    for round in range(card_draws):
+        last_card = fresh_card
+        previous_cards.append(last_card)
+        for card in last_card:
+            fresh_card = random.choice(deck_instance)
+            while fresh_card in previous_cards:
+                # print(previous_cards)
+                # print("Card already used:")
+                # print(fresh_card)
+                fresh_card = random.choice(deck_instance)
+                # print("New card:")
+                # print(fresh_card)
+        if last_card[:-1] < fresh_card[:-1]:
+            multiplier += 1
+            if multiplier == 5:
+                evaluator +=1
+                # print("success")
+    instances +=1
+print(f"Percentage of time better cards draw x5:    % {evaluator / instances}")
+
